4b17302a555f0bf67a7ddf0068d1b78deec4319a
Dayane Núbia <dayane.nubia67@gmail.com>
2022-08-11 22:49:48 -0300

    Add files via upload

---
+++ Aula 01/aula01.js
@@ -0,0 +1,7 @@
+function mudaCor() { 
+    titulo.style.color = 'red';
+    paragrafo.style.color = 'pink';
+}
+
+titulo.addEventListener('click', mudaCor);
+paragrafo.addEventListener('click', mudaCor);
